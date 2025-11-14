35e0e8046d4f3f68fbf3a71c1896d6a8cc6a7690
Dheeraj25-blr <dheerajdkblr@gmail.com>
2025-11-14 13:03:28 +0530

    Print your name,city

---
+++ Print your name, age, and city.py
@@ -0,0 +1,8 @@
+name = "Dheeraj"
+age = 22
+place = "Bengaluru"
+print("My name is", name )
+print("My age is", age)
+print("My place is", place)
+
+print(f"My name is {name}, My age is {age}, My place is {place}")
